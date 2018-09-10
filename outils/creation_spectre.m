135635cc919c6aaebe434b5e345d2e81c5aa7aad
adriensalon <42739018+adriensalon@users.noreply.github.com>
2018-09-10 11:36:33 +0200

    Update creation_spectre.m

--- outils/creation_spectre.m
+++ outils/creation_spectre.m
@@ -1,1 +1,10 @@
+[xSamples, sampleRate] = audioread('sample1.wav');
+N = sampleRate; % pour une précision de 1Hz
+xSamples = xSamples(1:N);
+amplitudes = abs(fft(xSamples, N));
+amplitudes = amplitudes(1:N/2);
+frequences = (1:N/2) .* (sampleRate/N);
 
+fid = fopen('sample1.txt','wt'); % améliorer pour meilleure lisibilité à la lecture
+fprintf(fid, '%f     %f\n', amplitudes, frequences);
+fclose(fid);
