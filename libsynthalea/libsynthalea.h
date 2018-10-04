6f2e79e2085913e6fd3432913d58295b80ee3382
adriensalon <42739018+adriensalon@users.noreply.github.com>
2018-10-04 17:49:04 +0200

    comme ça les const / pointeurs ?

--- libsynthalea/libsynthalea.h
+++ libsynthalea/libsynthalea.h
@@ -33,14 +33,14 @@ typedef struct spectre_analyse
 
 typedef struct banque_spectres
 {
-	spectre_analyse			collection_spectres[];
+	spectre_analyse				collection_spectres[];
 };
 
 typedef struct synthetiseur
 {
-	banque_oscillateurs		*oscillateurs;
-	banque_spectres			*spectres;
-	int						frequence_echantillonnage;
+	banque_oscillateurs			*oscillateurs;
+	banque_spectres				*spectres;
+	int					frequence_echantillonnage;
 	double					gain;
 };
 
@@ -53,7 +53,7 @@ void						charger_spectres(const char *url_collec, banque_spectres *bk_sp);
 void						ajouter_spectre_dans_la_collection(const spectre_analyse nv_sp, banque_spectres *bk_sp);
 void						sauvegarder_spectres(const char *url_collec, banque_spectres *bk_sp);
 
-void						calcul_collection_oscillateurs(const double idx_temp[], const double evo_puissance[], const double evo_disp[], const size_t nb_osc, const banque_spectres *bk_sp, banque_oscillateurs *bk_osc);
+void						calcul_collection_oscillateurs(const double idx_temp[], const double evo_puissance[], const double evo_disp[], const size_t nb_osc, const banque_spectres bk_sp, banque_oscillateurs *bk_osc);
 void						charger_oscillateurs(const char *url_collec, banque_oscillateurs *bk_osc);
 void						sauvegarder_oscillateurs(const char *url_collec, banque_oscillateurs *bk_osc);
 
