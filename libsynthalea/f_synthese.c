7f9b85c23ec9e27ddb5f66ddc07ac447f4b7a7e2
Adrien SALON <adrien.salon@live.fr>
2018-10-05 18:37:49 +0200

    structs

--- libsynthalea/f_synthese.c
+++ libsynthalea/f_synthese.c
@@ -9,11 +9,11 @@ double synthese(
 {
 	double 			t = i_echantillon / frequence_echantillonnage;
 	double 			somme_osc = 0;
-	for (unsigned i_osc = 0; i_osc < sizeof(bk_osc->comps) / sizeof(*bk_osc->comps); i_osc++)
+	for (unsigned i_osc = 0; i_osc < bk_osc->dim1; i_osc++)
 	{
 		double osc_ampl = 0;
 		double osc_freq = 0;
-		for (unsigned i_coef = 0; i_coef < sizeof(bk_osc->comps[i_osc]) / sizeof(*bk_osc->comps[i_osc]); i_coef++)
+		for (unsigned i_coef = 0; i_coef < bk_osc->dim2; i_coef++)
 		{			
 			double facteur_puissance = pow(t, i_coef);
 			osc_ampl += (bk_osc->comps[i_osc][i_coef]).ampl * velocite * facteur_puissance;
