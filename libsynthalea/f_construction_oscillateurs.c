97bb0735f2c475a4500c7a9dcaa30cfab942cf51
Adrien SALON <adrien.salon@live.fr>
2018-10-06 12:03:00 +0200

    interpolation

--- libsynthalea/f_construction_oscillateurs.c
+++ libsynthalea/f_construction_oscillateurs.c
@@ -17,10 +17,21 @@ void construction_oscillateurs(
 	unsigned nb_partiels; // > nb_osc
 	struct matAF *bk_sp_selec;
 
+
+
+	////////////////////////////////////////////////////////	Partie aleatoire : trouver le bon spectre pour chaque instant
+
 	// Matrice des spectres selectionnes et ordonnes par ordre chronologique
 	alloc_matAF(nb_instants, nb_partiels, bk_sp_selec);
 
-	// Suivi des voix a travers les spectres
+
+
+
+
+
+
+
+	////////////////////////////////////////////////////////	Suivi des voix a travers les spectres
 
 	// Matrice des oscillateurs
 	alloc_matAF(nb_osc, nb_instants, bk_osc);
@@ -35,45 +46,48 @@ void construction_oscillateurs(
 
 
 
-	// Interpolation
+	/////////////////////////////////////////////////////////	Interpolation
+
+	// Construction de la matrice de Vandermonde avec les idx_temps
+	alloc_matAF(nb_instants, nb_instants, matVandermonde);
+	for (unsigned i = 0; i < nb_instants; i++)
+	{
+		for (unsigned j = 0; j < nb_instants, j++)
+		{
+			// La matrice de Vandermonde est identique pour les amplitudes et les fréquences
+			(*matVandermonde).vecs[i].comps[j].ampl = pow(idx_temps[i], j);
+		}
+	}
+
+	// Decomposition V = L * U
+	for (unsigned j = 0; j < nb_instants; j++)
+	{
+		(*matL).vecs[j].comps[j].ampl = 1;
+		for (unsigned i = 0; i < j; i++)
+		{
+			somme = 0;
+			for (unsigned k = 0; k < j - 1; k++)
+			{
+				somme = somme + (*matL).vecs[i].comps[k].ampl * (*matU).vecs[k].comps[j].ampl;
+			}
+			(*matU).vecs[k].comps[j].ampl = (*matVandermonde).vecs[i].comps[j].ampl - somme;
+		}
+		for (unsigned i = j; i < nb_instants; i++)
+		{
+			somme = 0;
+			for (unsigned k = 0; k < i - 1; k++)
+			{
+				somme = somme + (*matL).vecs[i].comps[k].ampl * (*matU).vecs[k].comps[j].ampl;
+			}
+			(*matL).vecs[k].comps[j].ampl = ((*matVandermonde).vecs[i].comps[j].ampl - somme) / (*matU).vecs[j].comps[j];
+		}
+	}
+
+	// Resolution pour chaque oscillateur
+
 	for (unsigned i_osc = 0; i_osc < nb_osc; i_osc++)
 	{
 		// s_vecAF currentVec = vecteur ayant pour composantes les i_osc èmes composantes de chaque spectre
-		
-		// Construction de la matrice de Vandermonde avec les idx_temps
-		alloc_matAF(nb_instants, nb_instants, matVandermonde);
-		for (unsigned i = 0; i < nb_instants; i++)
-		{
-			for (unsigned j = 0; j < nb_instants, j++)
-			{
-				// La matrice de Vandermonde est identique pour les amplitudes et les fréquences
-				(*matVandermonde).vecs[i].comps[j].ampl = pow(idx_temps[i], j);
-			}
-		}
-
-		// Decomposition V = L * U
-		for (unsigned j = 0; j < nb_instants; j++)
-		{
-			(*matL).vecs[j].comps[j].ampl = 1;
-			for (unsigned i = 0; i < j; i++)
-			{
-				somme = 0;
-				for (unsigned k = 0; k < j - 1; k++)
-				{
-					somme = somme + (*matL).vecs[i].comps[k].ampl * (*matU).vecs[k].comps[j].ampl;
-				}
-				(*matU).vecs[k].comps[j].ampl = (*matVandermonde).vecs[i].comps[j].ampl - somme;
-			}
-			for (unsigned i = j; i < nb_instants; i++)
-			{
-				somme = 0;
-				for (unsigned k = 0; k < i - 1; k++)
-				{
-					somme = somme + (*matL).vecs[i].comps[k].ampl * (*matU).vecs[k].comps[j].ampl;
-				}
-				(*matL).vecs[k].comps[j].ampl = ((*matVandermonde).vecs[i].comps[j].ampl - somme) / (*matU).vecs[j].comps[j];
-			}
-		}
 
 		// Resolution par descente de L * y = currentVec
 
