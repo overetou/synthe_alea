d6856f207c12ad0ebfb7c2ca7856d08b1174d600
Adrien SALON <adrien.salon@live.fr>
2018-10-07 06:46:25 +0200

    a finir en priorité : - param double *gain a modifier si abs(somme_osc) > 1 - navigation dans la matrice - optimisation du code

--- libsynthalea/f_synthese.c
+++ libsynthalea/f_synthese.c
@@ -1,5 +1,7 @@
 #include <libsynthalea.h>
 
+#define PI 3.14159265358979323846
+
 double synthese(
 	unsigned		i_echantillon,
 	double			frequence,
@@ -7,20 +9,28 @@ double synthese(
 	unsigned		frequence_echantillonnage,
 	t_mat			*bk_osc)
 {
-	double 			t = i_echantillon / frequence_echantillonnage;
-	double 			somme_osc = 0;
-	for (unsigned i_osc = 0; i_osc < bk_osc->dim1; i_osc++)
+	double 			t;
+	double			osc_ampl;
+	double			osc_freq;
+	double			facteur_puissance;
+	double 			somme_osc;
+	unsigned		i_osc;
+	unsigned		i_coef;
+
+	t = i_echantillon / frequence_echantillonnage;
+	somme_osc = 0;
+	for (i_osc = 0; i_osc < bk_osc->dim1; i_osc++)
 	{
-		double osc_ampl = 0;
-		double osc_freq = 0;
-		for (unsigned i_coef = 0; i_coef < bk_osc->dim2; i_coef++)
+		osc_ampl = 0;
+		osc_freq = 0;
+		for (i_coef = 0; i_coef < bk_osc->dim2; i_coef++)
 		{			
-			double facteur_puissance = pow(t, i_coef);
+			facteur_puissance = pow(t, i_coef);
 			t_compAF composante = (*bk_osc).vecs[i_osc].comps[i_coef].compAF;
 			osc_ampl += composante.ampl * velocite * facteur_puissance;
 			osc_freq += composante.freq * frequence * facteur_puissance;
 		}
-		somme_osc += osc_ampl * sin(2 * 3.14 * osc_freq * t);
+		somme_osc += osc_ampl * sin(2 * PI * osc_freq * t);
 	}
 	return somme_osc;
 }
