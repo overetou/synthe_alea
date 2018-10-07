43aff43c2eaf66c36bbaa4149bfedd2d83e75331
Adrien SALON <adrien.salon@live.fr>
2018-10-07 06:12:14 +0200

    struct + union

--- libsynthalea/libsynthalea.h
+++ libsynthalea/libsynthalea.h
@@ -1,6 +1,9 @@
 #ifndef LIBSYNTHALEA
 #define LIBSYNTHALEA
 
+#include <stdbool.h>
+
+
 typedef struct				s_compAF
 {
     double					ampl;
@@ -8,31 +11,52 @@ typedef struct				s_compAF
 	struct s_compAF			*next;
 }							t_compAF;
 
-typedef struct				s_vecAF
+typedef struct				s_compDOUBLE
 {
-    t_compAF				*comps;    
-	unsigned				dim1;
-	struct s_vecAF			*next;
-}							t_vecAF;
+	double					val;
+	struct s_compDOUBLE		*next;
+}							t_compDOUBLE;
 
-typedef struct				s_matAF
+typedef struct				s_compBOOL
 {
-	t_vecAF					*vecs;
+	bool					val;
+	struct s_compBOOL		*next;
+}							t_compBOOL;
+
+typedef union s_comp
+{
+	t_compAF				compAF;
+	t_compDOUBLE			compDOUBLE;
+	t_compBOOL				compBOOL;
+}							t_comp;
+
+typedef struct				s_vec
+{
+    t_comp					*comps;   
+	unsigned				dim1;
+	struct s_vec			*next;
+}							t_vec;
+
+typedef struct				s_mat
+{
+	t_vec					*vecs;
 	unsigned				dim1;
 	unsigned				dim2;
-	//struct s_matAF		*next; Maybe later
-} 							t_matAF;
+} 							t_mat;
 
-void						alloc_matAF(unsigned dim1, unsigned dim2, t_matAF *mat);
-void						desalloc_matAF(t_matAF *mat);
 
-void						importer_spectre(const char *url_sp_brut, t_matAF *bk_sp);
-void						charger_spectres(const char *url_collec, t_matAF *bk_sp);
-void						sauvegarder_spectres(const char *url_collec, t_matAF *bk_sp);
+void						alloc_vec(unsigned dim1, t_vec *vec);
+void						alloc_mat(unsigned dim1, unsigned dim2, t_mat *mat);
+void						desalloc_vec(t_vec *vec);
+void						desalloc_mat(t_mat *mat);
 
-void						construction_oscillateurs(const double *idx_temps, const double *evo_puissance, const double *evo_dispersion, unsigned nb_osc, t_matAF *bk_sp, t_matAF *bk_osc);
-void						charger_oscillateurs(const char *url_collec, struct matAF *bk_osc);
+void						importer_spectre(const char *url_sp_brut, t_mat *bk_sp);
+void						charger_spectres(const char *url_collec, t_mat *bk_sp);
+void						sauvegarder_spectres(const char *url_collec, t_mat *bk_sp);
+
+void						construction_oscillateurs(const double *idx_temps, const double *evo_puissance, const double *evo_dispersion, unsigned nb_osc, const t_mat *bk_sp, t_mat *bk_osc);
+void						charger_oscillateurs(const char *url_collec, t_mat *bk_osc);
 void						sauvegarder_oscillateurs(const char *url_collec, struct matAF *bk_osc);
 
-double						synthese(unsigned idx_echantillon, double frequence, double velocite, unsigned frequence_echantillonnage, t_matAF *bk_osc);
+double						synthese(unsigned idx_echantillon, double frequence, double velocite, unsigned frequence_echantillonnage, t_mat *bk_osc);
 #endif
