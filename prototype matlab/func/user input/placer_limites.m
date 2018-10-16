901c179cbce84e64ff7df7f43926e4a65b645542
adriensalon <adrien.salon@live.fr>
2018-10-16 13:36:26 +0200

    matlab selection range

--- prototype matlab/func/user input/placer_limites.m
+++ prototype matlab/func/user input/placer_limites.m
@@ -5,10 +5,11 @@ function [echantillons] = placer_limites(echantillons)
     %   conserver pour garder un nombre entier de periodes
     
     figure;
-    plot(t, echantillons);
+    plot(echantillons);
+    axis([5000 6000 -1 1]);
     afficher_racines(echantillons);
-    [x_debut, ~] = ginput;
-    [x_fin, ~] = ginput;
+    [x_debut, ~] = ginput(1);
+    [x_fin, ~] = ginput(1);
     close;
     echantillons = echantillons(x_debut : x_fin);
 end
--- prototype matlab/func/util/afficher_racines.m
+++ prototype matlab/func/util/afficher_racines.m
@@ -5,9 +5,8 @@ function [] = afficher_racines(echantillons)
     hold on;
     for i = 2 : max(size(echantillons)) - 1
         if (echantillons(i - 1) > 0 && echantillons(i + 1) < 0) || (echantillons(i - 1) < 0 && echantillons(i + 1) > 0)
-            plot(i, 0, 'c');
+            errorbar(echantillons(i), 1);
         end
     end
     hold off;
 end
-
