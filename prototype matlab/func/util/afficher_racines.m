e1bc0604176e599a91238c0c817b2c4ae2dd82e7
adriensalon <adrien.salon@live.fr>
2018-10-16 13:39:24 +0200

    Update prototype matlab/func/util/afficher_racines.m

--- prototype matlab/func/util/afficher_racines.m
+++ prototype matlab/func/util/afficher_racines.m
@@ -5,7 +5,7 @@ function [] = afficher_racines(echantillons)
     hold on;
     for i = 2 : max(size(echantillons)) - 1
         if (echantillons(i - 1) > 0 && echantillons(i + 1) < 0) || (echantillons(i - 1) < 0 && echantillons(i + 1) > 0)
-            errorbar(echantillons(i), 1);
+            plot(i, 0, '*', 'LineWidth', 5);
         end
     end
     hold off;
